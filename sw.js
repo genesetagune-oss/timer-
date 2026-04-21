2046f866b0a708bc788f1d450b6b724a3a12c033
Genese Tugune <genesetugune@gmail.com>
2026-04-21 12:15:07 +0200

    Fix: replace Babel with htm for Safari iOS compatibility

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'interval-timer-v1';
+
+const PRECACHE = [
+  '/timer-/',
+  '/timer-/index.html',
+  '/timer-/manifest.json',
+  '/timer-/icon.svg',
+  'https://unpkg.com/react@18.3.1/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18.3.1/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone@7.25.6/babel.min.js',
+  'https://cdn.tailwindcss.com',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => Promise.allSettled(PRECACHE.map(url => c.add(url).catch(() => {}))))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
